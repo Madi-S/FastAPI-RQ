beba373b53a1a3b04177fea7e360ad72ba7358c7
Madi-S <khovansky99@gmail.com>
2021-08-07 19:28:35 +0600

    Endpoints enhanced

--- api.py
+++ api.py
@@ -10,23 +10,26 @@ app = FastAPI()
 redis_conn = Redis(host='myproj_redis', port=6379)
 q = Queue('my_queue', connection=redis_conn)
 
+
+class JobOut(BaseModel):
+    key: str
+    info: str
+
+
 @app.get('/hello')
 def test():
     '''Test endpoint'''
     return {'hello': 'world'}
 
 
-@app.post('/groups/{seconds}', status_code=201)
+@app.post('/groups/{seconds}', response_model=JobOut, status_code=201)
 def add_task(seconds: int):
     '''
     Adds tasks to worker queue.
     Expects body as dictionary matching the Group class.
     '''
-
     job = q.enqueue(sleep_task, seconds)
-    print(dir(job), job)
-
-    return {'job': job.__repr__, 'key': job.key}
+    return JobOut(info=job.__repr__(), key=job.key)
 
 
 @app.get('/query-size')
