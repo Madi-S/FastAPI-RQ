a4d0fe085f23e6b9c09a7971e906a7c0d280689e
Madi-S <khovansky99@gmail.com>
2021-08-09 11:59:34 +0600

    Chat with sockets implemented

--- routers/chat.py
+++ routers/chat.py
@@ -1,24 +1,17 @@
 import json
+from typing import List
 from datetime import datetime
-from fastapi import APIRouter, Request, HTTPException, WebSocket
+from fastapi import APIRouter, Request, HTTPException, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from worker import redis
-
-# TODO: Finish the HTML
-# TODO: Test and enhance the HTML
-# TODO: Store rooms and messages in redis (dictionaries or lists)
-# TODO: Create default rooms (no possibility to create or delete a room)
-# TODO: Implement socketio on client side
-# TODO: Implement socketio on server side
-
-CHAT_ROOMS = []
 
 
 class ChatRoom:
     def __init__(self, id):
         self.id = id
-        
+
     def save_message(self, text: str, author: str):
         message = ChatRoom.generate_message(text, author)
         json_message = json.dumps(message)
@@ -40,7 +33,7 @@ class ChatRoom:
             'user': author,
             'datetime': datetime.now()
         }
-     
+
 
 class ChatRoomsRegistry():
     def __init__(self):
@@ -59,6 +52,7 @@ class ChatRoomsRegistry():
             return chat_room
         raise KeyError(f'ChatRoom with id {id} is not registered')
 
+
 chat_rooms_registry = ChatRoomsRegistry()
 chat_rooms_registry.register('Room 666')
 chat_rooms_registry.register('Room 228')
@@ -71,7 +65,7 @@ templates = Jinja2Templates(directory='templates')
 @chat.get('/rooms', response_class=HTMLResponse)
 async def rooms(request: Request):
     chat_rooms_endpoints = chat_rooms_registry.get_all_endpoints()
-    
+
     return templates.TemplateResponse('rooms.html', {
         'request': request,
         'chat_rooms_endpoints': chat_rooms_endpoints
@@ -86,9 +80,56 @@ async def room(room_id: str, request: Request):
     except KeyError:
         messages = []
         raise HTTPException(404, f'Room {room_id} not found')
-    
+
     return templates.TemplateResponse('room.html', {
         'request': request,
         'room_id': room_id,
         'messages': messages
     })
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    @ChatRoom.save_message
+    async def broadcast_text(self, message: str, author: str):
+        for connection in self.active_connections:
+            await connection.send_text(f'{author}:{message}')
+
+    @ChatRoom.save_message
+    async def broadcast_json(self, message: dict):
+        for connection in self.active_connections:
+            await connection.send_json(json.dumps(message))
+
+
+manager = ConnectionManager()
+
+
+@chat.websocket('/ws/{room_id}/{client_id}')
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.broadcast_json({
+        'author': 'System',
+        'text': f'Client #{client_id} joined the chat'
+    })
+    await manager.connect(websocket)
+    try:
+        while True:
+            print(dir(websocket))
+            data_json = await websocket.receive_json()
+            await manager.broadcast_json(data_json)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast_json({
+            'author': 'System',
+            'text': f'Client #{client_id} left the chat'
+        })
---
+++ static/chat.js
@@ -0,0 +1,39 @@
+const username = prompt('Enter your username:', Date.now()).toString().trim()
+const ws = new WebSocket(`ws://localhost:5057/ws/${roomId}/${username}`)
+
+const messageInput = document.getElementById('message-input')
+const sendMessageBtn = document.getElementById('message-submit')
+const messagesCollection = document.getElementById('message-collection')
+sendMessageBtn.addEventListener('click', sendMessage)
+
+function sendMessage(event) {
+    const message = generateJSONMessage()
+    ws.send(message)
+    // appendMessage(message, 'You')
+    messageInput.value = ''
+    event.preventDefault()
+}
+
+function appendMessage(message) {
+    html = `
+    <li class="collection-item">
+            <div><b>${message.author}:</b> ${message.text}
+                <a class="secondary-content"><i class="material-icons">send</i></a>
+            </div>
+        </li>
+    `.trim()
+    messagesCollection.insertAdjacentHTML('beforeend', html)
+}
+
+function generateJSONMessage() {
+    const message = {}
+    message.author = username
+    message.text = messageInput.value.trim()
+    return JSON.stringify(message)
+}
+
+ws.onmessage = function (event) {
+    const message = JSON.parse(JSON.parse(event.data))
+    appendMessage(message)
+}
+
